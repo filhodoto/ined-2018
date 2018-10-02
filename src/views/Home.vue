cc6c38e8676e1c3f55aa55b8e0188afd5f9ba1f5
Gonçalo Ramalho <goncaloramalho@Goncalos-iMac-New.local>
2018-10-02 18:08:58 +0200

    ...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <article class='home'>
+        <img alt='Vue logo' src='../assets/logo.png'>
+        <section>
+            <p>somos constituidos por <strong>várias actividades</strong> que convergem na <strong>publicidade</strong> e o nosso alvo é <strong>promover as marcas dos clientes</strong></p>
+        </section>
+        <section>
+            <ul>
+                <li>Insta</li>
+                <li>Facebook</li>
+            </ul>
+        </section>
+    </article>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
